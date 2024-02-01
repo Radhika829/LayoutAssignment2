9ebcaf4144267ff50502de2d4c3082116c53f3d6
Radhika <radhikasn06@gmail.com>
2024-02-01 22:08:52 +0530

    first commit

---
+++ WebLayoutAssignment2/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebLayoutAssignment2.Models;
+
+namespace WebLayoutAssignment2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        static List<Customers> cus = new List<Customers>()
+        {
+          new Customers{CId=1,CName="Prashu",Salary=10000,Designation="HR" },
+          new Customers{CId=2,CName="Raju",Salary=90000,Designation="Developer" },
+          new Customers{CId=3,CName="John",Salary=80000,Designation="Manager" },
+          new Customers{CId=4,CName="Manu",Salary=70000,Designation="Tester" },
+        };
+        public ActionResult Index()
+        {
+            ViewBag.msg = "Customers Home Page";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewData["msg"] = "Success";
+            return View(new Customers());
+        }
+        [HttpPost]
+        public ActionResult Create(Customers cust)
+        {
+            if (ModelState.IsValid)
+            {
+                cus.Add(cust);
+                TempData["tempmsg"] = " New Customer registration success";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(cust);
+            }
+
+            
+        }
+        
+    }
+}
---
+++ WebLayoutAssignment2/Models/Customers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebLayoutAssignment2.Models
+{
+    public class Customers
+    {
+        public int CId { get; set; }
+        public string CName { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
